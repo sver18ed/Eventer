a27e160b53be52579bc44be28d9f62a269dfa07e
sver18ed <erik.svenningsson91@gmail.com>
2020-02-11 13:37:01 +0100

    ViewEventActivity

---
+++ app/src/main/java/com/example/eventer/ViewEventActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewEventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_event)
+    }
+}
