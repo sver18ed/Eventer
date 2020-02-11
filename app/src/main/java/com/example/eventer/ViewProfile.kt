9435b43e817752fcd28b4ab85b3f4aafba3bc07f
maan1686 <andreas.malmstroem@gmail.com>
2020-02-11 14:51:10 +0100

    Started with view profile

---
+++ app/src/main/java/com/example/eventer/ViewProfile.kt
@@ -0,0 +1,12 @@
+package com.example.eventer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+    }
+}
