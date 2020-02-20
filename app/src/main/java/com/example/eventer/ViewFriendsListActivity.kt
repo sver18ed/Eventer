56fb408cab6151d2a863a3315316ada216d2db3f
AndreasMalmstrom <andreas.malmstroem@gmail.com>
2020-02-20 11:08:59 +0100

    Created profile layout & started with view friends activity

---
+++ app/src/main/java/com/example/eventer/ViewFriendsListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewFriendsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_friends_list)
+    }
+}
--- app/src/main/java/com/example/eventer/ViewProfile.kt
+++ app/src/main/java/com/example/eventer/ViewProfile.kt
@@ -2,11 +2,27 @@ package com.example.eventer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.eventer.models.UserRepository
+import com.example.eventer.models.userRepository
 
 class ViewProfile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_profile)
+
+//
+//        val user = userRepository.getUserById(id)
+//
+//        val nameText = findViewById<TextView>(R.id.actual_name)
+//        val emailText = findViewById<TextView>(R.id.actual_email)
+//        val infoText = findViewById<TextView>(R.id.actual_info)
+//
+//        nameText.text = user!!.name
+//        emailText.text = user.email
+//        infoText.text = user.info
+
     }
+
 }
