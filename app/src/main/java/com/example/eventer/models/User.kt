0de535b648c85094a2b6d38466124a8d36208d83
sver18ed <erik.svenningsson91@gmail.com>
2020-03-04 16:33:34 +0100

    fixed user creation

--- app/src/main/java/com/example/eventer/models/User.kt
+++ app/src/main/java/com/example/eventer/models/User.kt
@@ -1,36 +1,9 @@
 package com.example.eventer.models
 
+import java.io.Serializable
+
 data class User (
-    val id: Int,
-    var name: String,
-    var email: String,
-    var info: String,
-    var friends: Array<User> = emptyArray()
+    val email: String = "",
+    val password: String = ""
 
-
-) {
-    override fun toString() = name
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as User
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (email != other.email) return false
-        if (info != other.info) return false
-        if (!friends.contentEquals(other.friends)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id
-        result = 31 * result + name.hashCode()
-        result = 31 * result + email.hashCode()
-        result = 31 * result + info.hashCode()
-        result = 31 * result + friends.contentHashCode()
-        return result
-    }
-}
+) : Serializable
